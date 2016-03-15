e530355659f21434f1b9b4eeddffa0a3e4b3cb29
JC <jbacampos@gmail.com>
2016-03-15 22:01:49 +0000

    Changed the HTML for the site.

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,17 +1,7 @@
 from django.shortcuts import render
-from django.template import RequestContext
-from django.core.context_processors import csrf
-
-def login_user(request):
-    dic = {}
-    dic.update(csrf(request))
-    dic[‘state’] = state
-    dic[‘username’] = username
-    return render_to_response(‘auth.html’, dic)
-
 
 # Create your views here.
 
-
-# the last three lines should look so
-
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+    
